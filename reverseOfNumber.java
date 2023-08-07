2b8b0a28578982c55cd803506bd99b22cc9b3ce8
Bishal-P <bishalprasad251122@gmail.com>
2023-08-08 02:42:06 +0530

    update reverseOfNumber.java

--- reverseOfNumber.java
+++ reverseOfNumber.java
@@ -6,6 +6,7 @@ public class reverseOfNumber {
         Scanner sc=new Scanner(System.in);
         System.out.print("Enter the number :");
         a=sc.nextInt();
+        int temp=a;
         int rev=0;
         while(a>0){
             int rem=a%10;
@@ -13,6 +14,7 @@ public class reverseOfNumber {
             a=a/10;
         }
         System.out.println("The reverse of the given number is :"+rev);
+        System.out.print("The number is palindrome or not :"+(temp==rev));
         sc.close();
     }
 }
