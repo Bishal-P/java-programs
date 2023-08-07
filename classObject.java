d0a6aed6ecacd9ecba9d6ee81522c7cdb82a5366
Bishal-P <bishalprasad251122@gmail.com>
2023-08-08 05:09:39 +0530

    update the file

---
+++ classObject.java
@@ -0,0 +1,10 @@
+public class classObject {
+    String a="Hello world";
+    void printdata(){
+        System.out.println("The value to print is this "+a);
+    }
+    public static void main(String[] args) {
+        classObject n=new classObject();
+        n.printdata();
+    }
+}
