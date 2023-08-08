ed934e5b0c7798289fd5daea7886ea6aee255625
Bishal-P <bishalprasad251122@gmail.com>
2023-08-09 01:41:19 +0530

    update constructors.java

--- constructors.java
+++ constructors.java
@@ -1,17 +1,16 @@
 public class constructors {
     public static void main(String[] args) {
         test a=new test();
+        System.out.println(test.test);
         
-    }
-
-
-    
+    } 
     
 }
 
 //we can not create an object with empty constructor if there is atleast one parameterized constructor 
 class test{
     int a;
+    static String test="hello this is a variable without object";
     test(int num){
         a=num;
     }
