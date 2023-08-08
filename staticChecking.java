15120cf29695d01082bd122e10bcf293f6ab4ac7
Bishal-P <bishalprasad251122@gmail.com>
2023-08-08 15:36:17 +0530

    static variable and functions

---
+++ staticChecking.java
@@ -0,0 +1,28 @@
+public class staticChecking {
+    public static void main(String[] args) {
+        Student a=new Student("Bishal", 1);
+        System.out.println("Static value ="+Student.college);
+        Student.college="BPPIMT";
+        System.out.println("Static value ="+Student.college);
+        Student b=new Student("Bishal2", 2);
+        System.out.println("Static value ="+b.college);
+        Student.changeCollegeName("aec");
+        System.out.println("Static value ="+a.college);
+
+    }
+}
+
+
+class Student{
+    String x;
+    int y;
+    static String college="Abc";
+    Student(String name,int roll){
+        x=name;
+        y=roll;
+    }
+    static void changeCollegeName(String newName){
+        college=newName;
+    }
+
+}
