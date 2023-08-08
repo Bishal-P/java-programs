fd04f4590cec54352b4f1c5d3d9ab74b7ab7818b
Bishal-P <bishalprasad251122@gmail.com>
2023-08-09 01:52:15 +0530

    iuhj

---
+++ inheritance.java
@@ -0,0 +1,29 @@
+class inheritance {
+
+public static void main(String[] args) {
+    second aa=new second("Bishal","btech");
+    aa.display();
+}
+    
+}
+
+
+class first{
+    String name,clas;
+    first(){
+        System.out.println("First");
+    }
+    
+}
+
+
+class second extends first{
+    second(String n,String c){
+        name=n;
+        clas=c;
+    }
+    
+    void display(){
+        System.out.println("The name ="+name+" class="+clas);
+    }
+}
