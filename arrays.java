4ddb336b98fb14299b655c24ab4987e72a262718
Bishal-P <bishalprasad251122@gmail.com>
2023-08-09 23:50:30 +0530

    update arrays.java and inheritance.java

--- arrays.java
+++ arrays.java
@@ -2,21 +2,21 @@ import java.util.Scanner;
 
 public class arrays {
     public static void main(String[] args) {
-        int arr[]=new int[10];
-        int i=0;
-        Scanner sc = new Scanner(System.in);
+        int     arr[] = new int[10];
+        int     i     = 0;
+        Scanner sc    = new Scanner(System.in);
         while(i<10){
             System.out.print("Enter the value of "+i+"th element :");
-            arr[i]=sc.nextInt();
+            arr[i] = sc.nextInt();
             i++;
         }
         sc.close();
-        i=0;
-        int sum=0;
+            i   = 0;
+        int sum = 0;
         System.out.print("The entered array is :");
         while(i<10){
             System.out.print(arr[i]+" ");
-            sum+=arr[i];
+            sum += arr[i];
             i++;
             
         }
