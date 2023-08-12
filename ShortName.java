45babf93e19a4a75d52c82ced267ba4d772ffcd8
Bishal <bishalprasad251122@gmail.com>
2023-08-12 21:21:00 +0530

    best code for short name

---
+++ ShortName.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class ShortName {
+    
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String shortname="";
+        String name="shiv kumar Prasad";
+        System.out.print("Enter Your name :");
+        name=sc.nextLine();
+        int flag=0,surname=0;
+        for(int i=name.length()-1;i>0;i--){
+            if(flag==1 && name.charAt(i-1)==' '){
+                surname=i;;
+                break;
+            }
+            if(name.charAt(i)!=' '){
+                flag=1;
+            }
+        }
+        for (int i=1;i<surname;i++){
+            if(i-1==0 && name.charAt(0)!=' '){
+                String val= Character.toString(name.charAt(0))+". ";
+                shortname=shortname.concat(val);    
+            }
+            else if(name.charAt(i-1)==' ' && name.charAt(i)!=' '){
+                String val=Character.toString(name.charAt(i))+". ";
+                shortname=shortname.concat(val);
+            }
+            
+        }
+        shortname=shortname.concat(name.substring(surname));
+        System.out.println("The shortname is "+shortname);
+    }
+}
---
+++ tempCodeRunnerFile.java
@@ -0,0 +1,9 @@
+r(int i=name.length()-1;i>=0;i--){
+        //     if(flag==1 && name.charAt(i-1)==' '){
+        //         flag=i;
+        //         break;
+        //     }
+        //     if(name.charAt(i)!=' '){
+        //         flag=1;
+        //     }
+        // }
