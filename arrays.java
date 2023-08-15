dee80d6136e673e5ca6a0b9d8c05d6e552a83245
Bishal P <120594033+Bishal-P@users.noreply.github.com>
2023-08-15 23:30:00 +0530

    Update arrays.java

--- arrays.java
+++ arrays.java
@@ -18,7 +18,6 @@ public class arrays {
             System.out.print(arr[i]+" ");
             sum += arr[i];
             i++;
-            
         }
         System.out.println();
         System.out.println("The sum of the array is :"+sum);
